e8b6c27234713f628505602fa166e6182db9cd9d
thejoryle <thejoryle@gmail.com>
2023-10-31 13:17:15 -0700

    Job class and parser implemented, untested

--- src/scheduler_simulator/InputParser.java
+++ src/scheduler_simulator/InputParser.java
@@ -1,8 +1,32 @@
 package scheduler_simulator;
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class InputParser {
-    Scanner scanner;
+    private final Scanner scanner;
+    private final List<Job> sortedJobList;
+
+    InputParser(File input) throws FileNotFoundException {
+        this.scanner = new Scanner(input);
+        this.sortedJobList = new ArrayList<>();
+    }
+
+    public List<Job> parse(){
+        int burst;
+        int arrival;
+        while(this.scanner.hasNext()){
+            burst = this.scanner.nextInt();
+            arrival = this.scanner.nextInt();
+            this.sortedJobList.add(new Job(burst, arrival));
+        }
+        this.sortedJobList.sort(new Job.sortByArrival());
+        for(int i = 0; i < this.sortedJobList.size(); i++){
+            this.sortedJobList.get(i).setJobNumber(i);
+        }
+
+        return this.sortedJobList;
+    }
 
 }
--- src/scheduler_simulator/Job.java
+++ src/scheduler_simulator/Job.java
@@ -1,4 +1,46 @@
 package scheduler_simulator;
 
+import java.util.Comparator;
+
 public class Job {
+    private final int arrivalTime;
+    private final int burstTime;
+    private int jobNumber;
+    private int remainingTime;
+    private int timeCompleted;
+    private int turnaroundTime;
+    private int waitingTime;
+
+    Job(int burstTime, int arrivalTime){
+        this.burstTime = burstTime;
+        this.arrivalTime = arrivalTime;
+        this.jobNumber = Integer.MIN_VALUE;
+        this.remainingTime = Integer.MIN_VALUE;
+        this.timeCompleted = Integer.MIN_VALUE;
+        this.turnaroundTime = Integer.MIN_VALUE;
+        this.waitingTime = Integer.MIN_VALUE;
+    }
+
+    public void setJobNumber(int newJobNumber){
+        this.jobNumber = newJobNumber;
+    }
+
+    public void decreaseRemainingTime(int decreaseBy){
+        this.remainingTime -= decreaseBy;
+    }
+
+    public void setTimeCompleted(int completedAt){
+        this.timeCompleted = completedAt;
+    }
+
+    public void calculateResults(){
+        this.turnaroundTime = this.timeCompleted - this.arrivalTime;
+        this.waitingTime = this.turnaroundTime - this.burstTime;
+    }
+
+    static class sortByArrival implements Comparator<Job>{
+        public int compare(Job a, Job b){
+            return a.arrivalTime - b.arrivalTime;
+        }
+    }
 }
